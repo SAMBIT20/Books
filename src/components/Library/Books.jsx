61b593e364bbcad474274b918770407f6adb012c
Sambit Kumar Nanda <sambitn20@gmail.com>
2021-12-18 17:16:05 +0530

    Adding Styles to Books Page and Scroll bar

--- src/components/Library/Books.jsx
+++ src/components/Library/Books.jsx
@@ -4,11 +4,32 @@ import "./Books.css";
 const Books = ({ allbooks }) => {
   console.log(allbooks);
   return (
-    <div>
+    <div className="books-container">
       {allbooks.map((data) => (
-        <div className="books-container">
-          <img src={data.img} alt="img" />
-          <h2>{data.title}</h2>
+        <div className="books-wrap">
+          <div class="product-card">
+            <div class="badge">NEW</div>
+            <div class="product-tumb">
+              <img src={data.img} alt="" />
+            </div>
+            <div class="product-details">
+              <span class="product-catagory">{data.genres}</span>
+              <h4>
+                <a href="">{data.title}</a>
+              </h4>
+              <p>{data.Author}</p>
+              <div class="product-bottom-details">
+                <div class="product-button">
+                  <button>Read Now</button>
+                </div>
+                <div class="product-links">
+                  <a href="">
+                    <i class="fa fa-heart"></i>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       ))}
     </div>
--- src/pages/Library/Library.jsx
+++ src/pages/Library/Library.jsx
@@ -1,11 +1,22 @@
 import React from "react";
 import Books from "../../components/Library/Books";
 import { data } from "../../config/Data";
+import "./Library.css";
 console.log(data);
 const Library = () => {
   return (
-    <div>
-      <h1>Boosks PAge</h1>
+    <div className="books-wrapper">
+      <div className="heading-books">
+        <h2>Digital Learning Ecosystem</h2>
+        <h3>BY EDUPRENEURS | FOR EDUCATORS | OF EDUCATION</h3>
+      </div>
+
+      <div className="Search-container">
+        <input type="text" placeholder="Type Book Name For Search" />
+        <button className="search-btn">
+          <i class="fas fa-search"></i>
+        </button>
+      </div>
       <Books allbooks={data} />
     </div>
   );
