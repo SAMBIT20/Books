70ed68a0a74bde2d835b51e99cc001ec0a28afcd
Sambit Kumar Nanda <sambit.kumar@bitcs.in>
2022-06-26 14:53:55 +0530

    add some books

--- src/config/Data.js
+++ src/config/Data.js
@@ -5,7 +5,8 @@ export const data = [
     Author: "Kyle Simpson ",
     img: "https://images2.imgbox.com/78/3a/aA9ImVxQ_o.jpg",
     genres: "Computer Science",
-    pdf: "https://drive.google.com/file/d/1Zp2NOrGDtCGtazNJzwLy1hhVt2tghHzC/preview",
+    pdf:
+      "https://drive.google.com/file/d/1Zp2NOrGDtCGtazNJzwLy1hhVt2tghHzC/preview",
   },
   {
     id: 2,
@@ -13,7 +14,8 @@ export const data = [
     Author: "Jon Duckett ",
     img: "https://images2.imgbox.com/68/11/RsygCJHn_o.jpg",
     genres: "Computer Science",
-    // pdf: "https://wtf.tw/ref/duckett.pdf",
+    pdf:
+      "https://drive.google.com/file/d/1H2xrmFWGYhzzj0E_bGtdRcCJlf1x12PI/preview",
   },
   {
     id: 3,
@@ -21,6 +23,8 @@ export const data = [
     Author: "Gayle Laakmann McDowell ",
     img: "https://images2.imgbox.com/a1/ac/C2u9KLoc_o.jpg",
     genres: "Computer Science",
+    pdf:
+      "https://drive.google.com/file/d/1WtJhNfDzcGlwrS_fbPFzb5btiHpYerDM/preview",
   },
   {
     id: 4,
@@ -28,7 +32,8 @@ export const data = [
     Author: "Narasimha Karumanchi ",
     img: "https://images2.imgbox.com/54/25/GBrI8T3p_o.jpg",
     genres: "Computer Science",
-    pdf: "https://www.docdroid.net/ZPfHmS5/data-structures-and-algorithms-narasimha-karumanchi-pdf",
+    pdf:
+      "https://www.docdroid.net/ZPfHmS5/data-structures-and-algorithms-narasimha-karumanchi-pdf",
   },
   {
     id: 5,
@@ -36,7 +41,8 @@ export const data = [
     Author: "Zed Shaw",
     img: "https://images2.imgbox.com/2b/89/S9teD06z_o.jpg",
     genres: "Computer Science",
-    pdf: "https://drive.google.com/file/d/1zYFxmsjT7pXe-qqxs0xJjfl-N7dgEXgg/preview",
+    pdf:
+      "https://drive.google.com/file/d/1zYFxmsjT7pXe-qqxs0xJjfl-N7dgEXgg/preview",
   },
   {
     id: 6,
@@ -44,6 +50,34 @@ export const data = [
     Author: "Kathy Sierra, Bert Bates",
     img: "https://images2.imgbox.com/0e/50/dsIyLlWP_o.jpg",
     genres: "Computer Science",
-    pdf: "https://docs.google.com/file/d/0BwxUBHTpU9kCU0xubVhyYlp0bWc/edit?resourcekey=0-sk68B4dt12P8MPoLieNBBA",
+    pdf:
+      "https://docs.google.com/file/d/0BwxUBHTpU9kCU0xubVhyYlp0bWc/edit?resourcekey=0-sk68B4dt12P8MPoLieNBBA",
+  },
+  {
+    id: 7,
+    title: "Structural analysis ",
+    Author: "C.S. Reddy Mc Graw Hill",
+    img: "https://images2.imgbox.com/27/2b/1UxcmnA9_o.jpg",
+    genres: "CIVIL ENGINEEERING",
+    pdf:
+      "https://drive.google.com/file/d/1Rs60TjAtU22jJMJImhJJYvMDupQEXgUL/preview",
+  },
+  {
+    id: 7,
+    title: "Digital System Design, 4th Edition,2008 ",
+    Author: "John Wakerley",
+    img: "https://images2.imgbox.com/29/2e/FGsSb1iG_o.jpg",
+    genres: "Electrical and Electronics Engineering",
+    pdf:
+      "https://drive.google.com/file/d/1yG6OLgTAMiCOAkPkouAC9La3x_ubTzYr/preview",
+  },
+  {
+    id: 7,
+    title: "Friction, wear, lubrication",
+    Author: "K.C Ludema",
+    img: "https://images2.imgbox.com/1b/4c/vMzeH6S9_o.jpg",
+    genres: "Mechanical Engineering",
+    pdf:
+      "https://drive.google.com/file/d/1ciKvlCOyqu3q4vJ-9HkqMzsLQl8WoMGH/preview",
   },
 ];
